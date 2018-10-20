4ba3d63f9afbdf086c9f7d19dacb56ad31b28cfa
Ajay Sharma <ajayks@msn.com>
2018-10-20 09:54:29 -0400

    jlkasdjfldskjfa

---
+++ WeAreTheUniverse/View/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  WeAreTheUniverse
+//
+//  Created by Ajay Sharma on 2018-10-20.
+//  Copyright © 2018 Waev. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
